a0b1a5a41fc911d206ee776bab0ddb44551afc2e
Sourab Hossain <sourabhossain.fci@gmail.com>
2022-08-15 14:24:01 +0600

    REF: #01 - Code Refactor

--- app.js
+++ app.js
@@ -1,79 +1,66 @@
-// supports expiration in 1 sec interval.
 class Redis {
-  #database;
-  tickEverySecond() {
-    setInterval(() => {
-      // current time
-      const time = Math.ceil(new Date().getTime() / 1000);
+	#database;
 
-      console.log(time);
+	#getCurrentTimePerSecond() {
+		return Math.ceil(new Date().getTime() / 1000);
+	}
 
-      if (this.expirationMap.has(time)) {
-        // expirationMap keys are time stamps
-        const valuesToDelete = this.expirationMap.get(time);
+	tickEverySecond() {
+		setInterval(() => {
+			const currentTimePerSecond = this.#getCurrentTimePerSecond();
 
-        // against expirationMap[time] we have array of keys that will expire in given time
-        valuesToDelete.forEach((key) => this.database.delete(key));
+			if (this.expirationMap.has(currentTimePerSecond)) {
+				// expirationMap keys are time stamps
+				const valuesToDelete =
+					this.expirationMap.get(currentTimePerSecond);
 
-        // optionally delete expirationMap[time]
-        this.expirationMap.delete(time);
-      }
-    }, 1000); // tick every second
-  }
+				// against expirationMap[time] we have array of keys that will expire in given time
+				valuesToDelete.forEach((key) => this.database.delete(key));
 
-  appendKeyInExpirationMap(timeIn10Sec, key) {
-    const list = this.expirationMap[timeIn10Sec];
-    if (!list) this.expirationMap.set(timeIn10Sec, [key]);
-    else list.push(key);
-    console.log(this.expirationMap);
-  }
+				// optionally delete expirationMap[time]
+				this.expirationMap.delete(currentTimePerSecond);
+			}
+		}, 1000); // tick every second
+	}
 
-  constructor() {
-    this.database = new Map();
-    this.expirationMap = new Map();
-    this.tickEverySecond();
-  }
+	appendKeyInExpirationMap(timeIn10Sec, key) {
+		const list = this.expirationMap[timeIn10Sec];
 
-  set(key, value, durationIn10Sec, callback = null) {
-    this.database.set(key, value);
-    this.appendKeyInExpirationMap(
-      Math.ceil(new Date().getTime() / 1000) + durationIn10Sec,
-      key
-    );
+		if (list) {
+			list.push(key);
+			return;
+		}
 
-    callback?.();
-  }
+		this.expirationMap.set(timeIn10Sec, [key]);
+	}
 
-  get(key, callback = null) {
-    const value = this.database.get(key);
-    callback?.(value);
-    return value;
-  }
+	constructor() {
+		this.database = new Map();
+		this.expirationMap = new Map();
+		this.tickEverySecond();
+	}
 
-  del(key, callback = null) {
-    this.database.delete(key);
-    callback?.();
-  }
+	set(key, value, duration) {
+		this.database.set(key, value);
+		const currentTimePerSecond = this.#getCurrentTimePerSecond();
+		this.appendKeyInExpirationMap(currentTimePerSecond + duration, key);
+	}
 
-  quit(callback = null) {
-    this.database.clear();
-    callback?.();
-  }
+	get(key) {
+		return this.database.get(key);
+	}
 
-  getAllKeys() {
-    return this.database.keys();
-  }
+	del(key) {
+		this.database.delete(key);
+	}
+
+	quit() {
+		this.database.clear();
+	}
+
+	getAllKeys() {
+		return this.database.keys();
+	}
 }
 
-// module.exports = Redis;
-
-let object = new Redis();
-const time = new Date().getTime();
-
-object.set("name", "John Doe", 7);
-object.set("subject", "System Deign", 5);
-object.set("roll", "1001", 3);
-
-setInterval(() => {
-  console.log("----->::: ", object.getAllKeys());
-}, 1000);
+module.exports = new Redis();
